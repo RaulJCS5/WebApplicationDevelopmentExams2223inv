169917ac96317b249698be5bc12ef0d381db42af
RaulJCS5 <rauljosesantos@hotmail.com>
2023-01-17 15:13:36 +0000

    Update PendingController.kt

--- exam/src/main/kotlin/isel/daw/study/exam/ver2122/PendingController.kt
+++ exam/src/main/kotlin/isel/daw/study/exam/ver2122/PendingController.kt
@@ -1,5 +1,6 @@
 package isel.daw.study.exam
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,22 +11,31 @@ import kotlin.concurrent.withLock
 class PendingController(
     val pendingService: PendingService
 ) {
+    private val pendingRequests = mutableMapOf<String, Int>()
+
     @GetMapping("/pending")
-    fun pending():String{
-        return pendingService.getPending().toString()
+    fun pending():Map<String, Int>{
+        return pendingService.getPending()
+    }
+
+    @GetMapping("/simplepending")
+    fun getPendingRequests(request: HttpServletRequest): Map<String, Int> {
+        val method = request.method
+        pendingRequests[method] = pendingRequests.getOrDefault(method, 0) + 1
+        return pendingRequests
     }
 }
 
 @Service
 class PendingService{
-    private val list = mutableListOf<String>()
+    private val pendingRequests = mutableMapOf<String, Int>()
     private val lock = ReentrantLock()
     fun addPending(p:String){
         lock.withLock {
-            list.add(p)
+            pendingRequests[p] = pendingRequests.getOrDefault(p, 0) + 1
         }
     }
-    fun getPending():MutableList<String>{
-        return list
+    fun getPending():MutableMap<String,Int>{
+        return pendingRequests
     }
 }
